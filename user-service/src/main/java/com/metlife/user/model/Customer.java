90a865ddc6ea9fda151f1b56c1c5da9a4f0b9dc8
karthikng <karthikng777@gmail.com>
2025-09-27 15:25:30 +0530

    new userlogin

--- user-service/src/main/java/com/metlife/user/model/Customer.java
+++ user-service/src/main/java/com/metlife/user/model/Customer.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.mongodb.lang.NonNull;
+
 import lombok.Data;
 
 @Data
@@ -12,15 +14,24 @@ import lombok.Data;
 public class Customer {
 	
     @Id
+	@NonNull
     private String customerId;
+	@NonNull
     private String name;
+	@NonNull
     private String email;
+	@NonNull
     private String phone;
+	@NonNull
     private String insurancePlan;
+	@NonNull
     private LocalDateTime registrationDate;
+	@NonNull
     private boolean isInsured;
+	@NonNull
     private double totalClaims;
+	@NonNull
     private double totalPremium;
-    private int rewardPoints;
+
 
 }
--- user-service/src/main/java/com/metlife/user/model/CustomerAdditionalDetails.java
+++ user-service/src/main/java/com/metlife/user/model/CustomerAdditionalDetails.java
@@ -3,6 +3,8 @@ package com.metlife.user.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.mongodb.lang.NonNull;
+
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,16 +15,28 @@ import lombok.Setter;
 @Document(collection = "users")
 public class CustomerAdditionalDetails {
     @Id
+    @NonNull
     private String id;
+    @NonNull
     private String name;
+    @NonNull
     private int age;
+    @NonNull
     private String sex;
+    @NonNull
     private double totalCholesterol;
+    @NonNull
     private double ldl;
+    @NonNull
     private double hdl;
+    @NonNull
     private int systolicBp;
+    @NonNull
     private int diastolicBp;
-    private Boolean smoking;   // 0=No, 1=Yes
-    private Boolean diabetes;  // 0=No, 1=Yes
+    @NonNull
+    private Boolean smoking;  
+    @NonNull // 0=No, 1=Yes
+    private Boolean diabetes; 
+    @NonNull // 0=No, 1=Yes
     private Boolean heartAttack; // 0=No, 1=Yes
 }
